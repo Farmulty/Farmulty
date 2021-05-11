f6aad3472e8bba79b9b66e24871e98eab82f3367
tomGER <github@tomger.eu>
2021-05-11 19:35:14 +0200

    Implement Carrots crop

---
+++ scripts/plants/Carrot.gd
@@ -0,0 +1,18 @@
+extends StaticBody2D
+
+signal is_mature
+
+export var Stage: int
+var creation_date: int
+
+func _ready():
+	creation_date = OS.get_unix_time()
+
+func _on_Timer_timeout():
+	Stage += 1
+	
+	$AnimatedSprite.frame = Stage
+	
+	if Stage == 5:
+		emit_signal("is_mature")
+		$Timer.stop()
