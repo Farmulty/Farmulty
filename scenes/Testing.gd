8c43d0307488451a1465382ede78e80001517e79
tomGER <github@tomger.eu>
2021-05-13 00:39:54 +0200

    Remove SimpleSave plugin
    Honestly, it really didn't work well enough for my needs. I'll create my own saving/loading feature in the long term probably.

--- scenes/Testing.gd
+++ scenes/Testing.gd
@@ -1,15 +1,6 @@
 extends Node2D
 
-onready var save = SimpleSave.new()
-
 func _on_Button_pressed():
 	$Character.plant_crop("carrots")
-func _on_Save_pressed():
-	save.save_scene(get_tree(), "res://saves/test.tscn")
-
-
-func _on_Load_pressed():
-	save.load_scene(get_tree(), "res://saves/test.tscn")
-
 func _process(delta):
 	$Path2D/PathFollow2D.offset += 15 * delta
