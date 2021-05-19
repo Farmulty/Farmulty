590292c2e7f947e750ac99c46157b1aec9414a33
tomGER <github@tomger.eu>
2021-05-19 15:26:13 +0200

    Generate flower placements dynamically

--- scenes/Overworld_Tilemaps.gd
+++ scenes/Overworld_Tilemaps.gd
@@ -1,10 +1,22 @@
 extends CanvasLayer
 
-var x: int = 100
-var y: int = 100
+var flower_amount: int = 220
+var grass_amount: int = 30
+
+var x_r: int = 70
+var y_r: int = 50
 
 func plant_randomly():
+	randomize()
+
 	var flowers = get_node("Flowers")
+
+	for flower in range(flower_amount + 1):
+		var x = randi() % x_r
+		var y = randi() % y_r
+		var type = randi() % 4
+
+		flowers.set_cell(x, y, 16, false, false, false, Vector2(type, 1))
 
 func _ready():
 	plant_randomly()
