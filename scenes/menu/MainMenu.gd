971e4b8a1e45610dfd002161135bbf51fe25793a
tomGER <github@tomger.eu>
2021-05-20 17:38:15 +0200

    Structure cleanup

--- scenes/menu/MainMenu.gd
+++ scenes/menu/MainMenu.gd
@@ -3,7 +3,7 @@ extends Control
 onready var global = get_node("/root/Global")
 
 func _on_Start_pressed():
-	global.goto_scene("res://scenes/Overworld.tscn")
+	global.goto_scene("res://scenes/overworld/Overworld.tscn")
 
 func _on_Quit_pressed():
 	get_tree().quit()
